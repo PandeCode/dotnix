1e6bdd4fc8c123cca9e15e744bf633acbfd82b3b
PandeCode <pandeshawnbenjamin@gmail.com>
2025-09-30 22:00:15 -0400

    first cache

---
+++ README.md
@@ -0,0 +1,1 @@
+this will be a branch to host a flake that builds specific packages with actions and caches to cachix so that i dont build anything locally
---
+++ flake.nix
@@ -0,0 +1,131 @@
+rec {
+  description = "nix config";
+
+  nixConfig = {
+    trusted-users = ["root" "shawn"];
+    experimental-features = ["nix-command" "flakes" "pipe-operators"];
+    accept-flake-config = true;
+    show-trace = true;
+    auto-optimise-store = true;
+
+    # substituters = ["https://aseipp-nix-cache.freetls.fastly.net"];
+
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://charon.cachix.org"
+      "https://niri.cachix.org"
+      # "https://cuda-maintainers.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "charon.cachix.org-1:epdetEs1ll8oi8DT8OG2jEA4whj3FDbqgPFvapEPbY8="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+      # "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+  };
+
+  inputs = {
+    self.submodules = true;
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    # nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+
+    # darwin = {
+    #   url = "github:LnL7/nix-darwin";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+    hypr-dynamic-cursors = {
+      url = "github:VirtCode/hypr-dynamic-cursors";
+      inputs.hyprland.follows = "hyprland";
+    };
+
+    # xmonad-contexts = {
+    #   url = "github:Procrat/xmonad-contexts";
+    #   flake = false;
+    # };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zjstatus = {
+      url = "github:dj95/zjstatus";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hermes = {
+      url = "github:pandecode/hermes";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    obolc = {
+      url = "github:PandeCode/obolc";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+    };
+    stylix.url = "github:danth/stylix";
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ghostty = {
+      url = "github:ghostty-org/ghostty";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+  in {
+    nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+    devShells = {
+      ${system}.default = nixpkgs.legacyPackages.${system}.mkShell {
+        buildInputs = with inputs; [
+          # inputs.charon-shell.packages.${system}.default
+          # inputs.obolc.packages.${system}.default
+          # inputs.hyprland.packages.${system}.hyprland;
+          # inputs.hermes.packages.${system}.default
+          inputs.niri.packages.${system}.niri-unstable
+          inputs.ghostty.packages.${system}.default
+          inputs.zjstatus.packages.${system}.default
+        ];
+      };
+    };
+  };
+}
