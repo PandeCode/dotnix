b480c8d33799174762974d3e71f1f7c9ab390068
PandeCode <pandeshawnbenjamin@gmail.com>
2025-11-19 12:40:57 -0500

    failed ci

--- derivations/default.nix
+++ derivations/default.nix
@@ -1,6 +1,6 @@
 callPackage: {
   sddm-custom-theme = callPackage ../derivations/sddm-custom-theme.nix {};
-  plymouth-theme-custom = callPackage ../derivations/plymouth-theme-custom.nix {};
+  # plymouth-theme-custom = callPackage ../derivations/plymouth-theme-custom.nix {};
   plymouth-theme-cat = callPackage ../derivations/plymouth-theme-cat.nix {};
   notify-send-py = callPackage ../derivations/notify-send-py.nix {};
 }
