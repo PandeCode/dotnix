b98a6d3da1bb7109e323feca2c410ca639f42b3d
PandeCode <pandeshawnbenjamin@gmail.com>
2026-04-03 21:09:36 -0400

    niri

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,21 +1,22 @@
 inputs:
 with inputs; {
   nixpkgs.overlays = [
-    (final: prev: {
+    nix-matlab.overlay
+    (_final: prev: {
       ghostty = ghostty.packages.${prev.system}.default;
 
-      niri-unstable = prev.niri-unstable.overrideAttrs (old: rec {
-        src = prev.fetchFromGitHub {
-          owner = "niri-wm";
-          repo = "niri";
-          rev = "refs/pull/3483/head";
-          hash = "sha256-ZiGGjRL2H67GcL6BvZV99khW++aHpJ2NA4n71qZiJ9A=";
-        };
-        cargoDeps = final.rustPlatform.fetchCargoVendor {
-          inherit src;
-          hash = "sha256-Fv3uClwuuAAGTQ7ujuAQW7xCoYFCw4q9QC08Z7Q7Hdk=";
-        };
-      });
+      # niri-unstable = prev.niri-unstable.overrideAttrs (_old: rec {
+      #   src = prev.fetchFromGitHub {
+      #     owner = "niri-wm";
+      #     repo = "niri";
+      #     rev = "refs/pull/3483/head";
+      #     hash = "sha256-ZiGGjRL2H67GcL6BvZV99khW++aHpJ2NA4n71qZiJ9A=";
+      #   };
+      #   cargoDeps = final.rustPlatform.fetchCargoVendor {
+      #     inherit src;
+      #     hash = "sha256-Fv3uClwuuAAGTQ7ujuAQW7xCoYFCw4q9QC08Z7Q7Hdk=";
+      #   };
+      # });
     })
   ];
 }
