fa37612bf758fa69f547d96c2f413e4559759918
PandeCode <pandeshawnbenjamin@gmail.com>
2026-08-10 14:27:33 -0400

    zig is so cool

--- bin/environ2zig.zig
+++ bin/environ2zig.zig
@@ -2,59 +2,16 @@
 
 const std = @import("std");
 
-fn escape(gpa: std.mem.Allocator, url: []const u8) ![]const u8 {
-    // "\n"Newline
-    // "\r"Carriage Return
-    // "\t"Tab
-    // "\\"Backslash
-    // "\'"Single Quote
-    // "\""Double Quote
-    // "\xNN"hexadecimal 8-bit byte value (2 digits)
-    // "\u{NNNNNN}"hexadecimal Unicode scalar value UTF-8 encoded (1 or more digits)
-    // }
-
-    const chars = .{
-        .{ "\\", "\\\\" },
-        .{ "\t", "\\t" },
-        .{ "\n", "\\n" },
-        .{ "\r", "\\r" },
-        .{ "\"", "\\\"" },
-    };
-
-    var expansions: usize = 0;
-    inline for (chars) |t| {
-        expansions += (t[1].len - t[0].len) * std.mem.count(u8, url, t[0]);
-    }
-    var start_output = try gpa.alloc(u8, url.len + expansions);
-    var end = url.len;
-    @memcpy(start_output[0..end], url);
-
-    inline for (chars) |t| {
-        const output = try std.mem.replaceOwned(u8, gpa, start_output[0..end], t[0], t[1]);
-        end = output.len;
-        @memcpy(start_output[0..end], output);
-        gpa.free(output);
-    }
-
-    return start_output;
-}
-
 pub fn main(init: std.process.Init) !void {
-    const gpa = init.arena.allocator();
-
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.Io.File.stdout().writer(init.io, &stdout_buffer);
+    var stdout_writer = std.Io.File.stdout().writer(init.io, &.{});
     const stdout = &stdout_writer.interface;
 
-    const envMap = try init.minimal.environ.createMap(gpa);
-    var envIter = envMap.iterator();
+    var envIter = init.environ_map.iterator();
 
     while (envIter.next()) |env| {
-        const key = env.key_ptr.*;
-        const val = try escape(gpa, env.value_ptr.*);
-        if (std.zig.isValidId(key)) {
-            try stdout.print("pub const {s} = \"{s}\";\n", .{ key, val });
-        } else try stdout.print("pub const @\"{s}\" = \"{s}\";\n", .{ key, val });
+        try stdout.print("pub const {f} = \"", .{std.zig.FormatId{ .bytes = env.key_ptr.*, .flags = .{} }});
+        try std.zig.stringEscape(env.value_ptr.*, stdout);
+        try stdout.writeAll("\";\n");
     }
 
     try stdout.flush();
